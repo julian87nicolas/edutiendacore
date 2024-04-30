e7ceccc7c1c252efd25af57df0df4190f8336c2d
Julian <julian.camargo@sitrack.com>
2024-04-29 22:10:16 -0300

    chore: addProducts implementation

--- src/main/java/edu/tienda/core/services/product/ProductServiceImpl.java
+++ src/main/java/edu/tienda/core/services/product/ProductServiceImpl.java
@@ -34,7 +34,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public void addProduct(Product product) {
-
+        products.add(product);
     }
 
     @Override
