cae24f8ca13eec2d8859d9e7434912c33cf0fc90
Julian <julian.camargo@sitrack.com>
2024-05-07 18:37:36 -0300

    refactor: added lombok

--- src/main/java/edu/tienda/core/configurations/ConfigurationParameters.java
+++ src/main/java/edu/tienda/core/configurations/ConfigurationParameters.java
@@ -1,47 +1,17 @@
 package edu.tienda.core.configurations;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Data
 @ConfigurationProperties(prefix = "app")
 public class ConfigurationParameters {
     private String name;
     private String country;
     private String author;
     private String language;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
 
     @Override
     public String toString() {
