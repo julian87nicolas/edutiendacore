2c88584d243566e1498ec12e6b7ca60312fd6cb1
Julian <julian.camargo@sitrack.com>
2024-05-11 16:54:27 -0300

    chore: added query methods

--- src/main/java/edu/tienda/core/persistance/repositories/FavListRepository.java
+++ src/main/java/edu/tienda/core/persistance/repositories/FavListRepository.java
@@ -4,6 +4,11 @@ import edu.tienda.core.persistance.entities.FavListEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface FavListRepository extends JpaRepository<Integer, FavListEntity> {
+public interface FavListRepository extends JpaRepository<FavListEntity, Integer> {
+    FavListEntity findFavListEntityById(Integer id);
+    FavListEntity findFavListEntityByUsernameAndName(String username, String name);
+    List<FavListEntity> findFavListEntitiesByUsername(String username);
 }
